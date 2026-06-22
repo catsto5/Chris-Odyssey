f7fb034c507fdb42ae862838c519f6597de3b4e4
catsto5 <christopherdavid6988@gmail.com>
2026-06-22 23:08:07 +0800

    Update Christopher odyssey.py

--- Christopher odyssey.py
+++ Christopher odyssey.py
@@ -1,15 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 #================================================
 #PARAMETERS
 #================================================
-m=100 #how many dynamical systems --- l=1,...,m
+m=500 #how many dynamical systems\
 T=100
 N=100
 sigma=1.0
 beta=1
 sigma2=[0.5,1.0,1.5,2.0,2.5]
-beta2=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
 
 #================================================
 #GENERATE c_l = F(l/m) 
@@ -23,7 +23,10 @@ c=F(l/m)
 #GENERATE \bar{f^*_l} 
 #================================================
 def g(x):
-  return np.sqrt(2)*np.sin(2*np.pi*x)
+  if 0<=x<0.5:
+      return x
+  elif 0.5<=x<=1:
+      return -x
 def bar_f(x,c): #dependent on l
   return c*g(x)
 
@@ -45,9 +48,18 @@ for i in range(m):
     x[i,t+1]=f(x[i,t],c_l)+eta[i,t]
 
 #================================================
-#PLOTS
+#SANITY CHECK
 #================================================
+beta2=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0] #values ot beta to try
+m2=np.arange(10,m+1) #values of m to try
 
-
-#================================================
-
+for beta in beta2:
+    y = []
+    for m in m2:
+        c = (np.arange(1, m+1)/m)**beta
+        y.append(np.sum((c[:-1] - c[1:])**2))
+    plt.plot(m2, y, label=f"β={beta}")
+plt.xlabel(r"$m$")
+plt.ylabel(r"$\sum_{l=1}^{m-1} |c_l - c_{l+1}|^2$")
+plt.legend()
+plt.show()
